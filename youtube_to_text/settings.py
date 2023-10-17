7f3cf1c9c18e90d36d1144c3399eb187bb52401b
Justin Lee <joonsmoons@gmail.com>
2023-10-17 23:43:54 +0900

    add header descriptions

--- youtube_to_text/settings.py
+++ youtube_to_text/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = ["web-production-3640.up.railway.app", "127.0.0.1"]
+# CSRF_TRUSTED_ORIGINS = ["https://web-production-3640.up.railway.app"]
 
 # Application definition
 
