2c77920d41c22fe6ce79d4c1cd1c214464fe52d6
Justin Lee <joonsmoons@gmail.com>
2023-10-17 23:30:23 +0900

    Added files and changes required for deployment

--- youtube_to_text/settings.py
+++ youtube_to_text/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +133,11 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+
+if "DATABASE_URL" in os.environ:
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
