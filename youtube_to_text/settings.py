52123b81e4fd05bb34dd52b487d3c0523657e392
Justin Lee <joonsmoons@gmail.com>
2023-10-18 09:28:43 +0900

    Add hosts

--- youtube_to_text/settings.py
+++ youtube_to_text/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["web-production-3640.up.railway.app", "127.0.0.1"]
-# CSRF_TRUSTED_ORIGINS = ["https://web-production-3640.up.railway.app"]
+
+ALLOWED_HOSTS = ["youtube-to-text-production.up.railway.app", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://youtube-to-text-production.up.railway.app/"]
 
 # Application definition
 
