6b794c89a0aa6bb8018539d8c952df7f25e74193
Justin Lee <joonsmoons@gmail.com>
2023-11-29 13:20:59 +0900

    Add more security measures

--- youtube_to_text/settings.py
+++ youtube_to_text/settings.py
@@ -38,9 +38,19 @@ ALLOWED_HOSTS = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://youtube-to-text.com",
-    "https://*.youtube-to-text.com",
+    "https://www.youtube-to-text.com",
     "https://youtube-to-text-production.up.railway.app",
 ]
+
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # Application definition
 
